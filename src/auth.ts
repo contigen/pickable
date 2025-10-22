f90628e0b07ac6a58a1aac42b551c93afc926e3d
contigen <69436315+contigen@users.noreply.github.com.>
2025-10-22 21:56:48 +0100

    Optimize middleware for Edge runtime size limits

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,6 @@ import { loginSchema } from '@/lib/schema'
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import { getUser } from '@/lib/db-queries'
-import { NextResponse } from 'next/server'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -28,24 +27,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   callbacks: {
-    async authorized({ request: req, auth }) {
-      const PUBLIC_ROUTES = [`/`, ``, `/onboarding`]
-      const { pathname } = req.nextUrl
-      const isLoggedIn = !!auth?.user
-      const isAPublicRoute = PUBLIC_ROUTES.some(route => route === pathname)
-      if (isLoggedIn && pathname === '/') {
-        const searchParams = req.nextUrl.searchParams
-        const callbackURL = searchParams.get('callbackUrl')
-        if (callbackURL) {
-          return NextResponse.redirect(new URL(callbackURL, req.url))
-        }
-        return NextResponse.redirect(new URL(`/dashboard`, req.url))
-      }
-      if (isAPublicRoute) {
-        return true
-      }
-      return isLoggedIn
-    },
     async jwt({ token, user }) {
       if (user) {
         token = { ...token, ...user }
